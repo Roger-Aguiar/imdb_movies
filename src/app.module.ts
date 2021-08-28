35aad1ea99f1973365de17c96fc8139cce94b879
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2021-08-28 16:43:56 -0300

    It makes the connection with the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ActorsModule } from './actors/actors.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ActorsModule],
+  imports: [ActorsModule, 
+            TypeOrmModule.forRoot({
+              type: 'mysql',
+              host: 'localhost',
+              port: 3306,
+              username: 'root',
+              password: '983453069',
+              database: 'imdb_movies123',
+              entities: [],
+              synchronize: true,
+            }),
+          ],
   controllers: [AppController],
   providers: [AppService],
 })
