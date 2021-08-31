6e346a9b5d5b9a40a55c3ca39d52ff5befb5ee13
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2021-08-31 14:21:13 -0300

    It creates creates the properties for NewMovieDto

--- src/movies/dto/new-movie.dto.ts
+++ src/movies/dto/new-movie.dto.ts
@@ -1,1 +1,11 @@
-export class NewMovieDto {}
+export class NewMovieDto 
+{
+    id: number;
+    original_title: string;
+    title: string;
+    year: string;
+    duration: string;
+    imdb_link: string;
+    register_date: string;
+    last_update: string;
+}
