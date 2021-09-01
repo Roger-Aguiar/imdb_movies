3dde8be1b3d10695bdcdd2138b6dc3a1cf49b5ea
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2021-09-01 08:48:35 -0300

    It adds Genre entity into the movies.module.

--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { MoviesController } from './movies.controller';
 import { Actor } from '../actors/entities/actor.entity';
+import { Genre } from '../genres/entities/genre.entity';
+
 @Module(
   {
-    imports: [Actor],
+    imports: [Actor, Genre],
     controllers: [MoviesController],
     providers: [MoviesService]
   })
