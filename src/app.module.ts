ac1f413cfb5a13d734634b2c0894d5821f7a63e3
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2021-09-01 08:19:45 -0300

    It adds the GenresModule into the imports.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ActorsModule } from './actors/actors.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MoviesModule } from './movies/movies.module';
+import { GenresModule } from './genres/genres.module';
 
 @Module({
-  imports: [ActorsModule, TypeOrmModule.forRoot(), MoviesModule],
+  imports: [ActorsModule, TypeOrmModule.forRoot(), MoviesModule, GenresModule],
   controllers: [AppController],
   providers: [AppService],
 })
